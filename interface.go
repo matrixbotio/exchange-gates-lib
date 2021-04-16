b373d450b6016dd6723cf6369d2912eb5986173e
Sagleft <sagleft@inbox.ru>
2021-04-16 21:05:13 +0300

    add new structs

--- interface.go
+++ interface.go
@@ -4,8 +4,8 @@ import "github.com/matrixbotio/errors"
 
 //ExchangeInterface - universal exchange adapter interface
 type ExchangeInterface interface {
-	getOrderData() (*structs.TradeEventData, *errors.APIError)
-	placeOrder(order structs.BotOrder) (*struct{}, *errors.APIError)
+	getOrderData() (*TradeEventData, *errors.APIError)
+	placeOrder(order BotOrder) (*struct{}, *errors.APIError)
 	getAccountData() (*struct{}, *errors.APIError)
 	getPairLastPrice() (float64, *errors.APIError)
 	cancelPairOrder() *errors.APIError
--- structs.go
+++ structs.go
@@ -1,1 +1,20 @@
 package matrixgates
+
+/*
+BotOrder - structure containing information about the order placed by the bot.
+Used when auto-resuming trades
+*/
+type BotOrder struct {
+	Type    string  `json:"type"`
+	Qty     float64 `json:"qty"`
+	Price   float64 `json:"price"`
+	Deposit float64 `json:"deposit"`
+}
+
+//TradeEventData - container for bot trading new event data
+type TradeEventData struct {
+	OrderID        int64
+	OrderAwaitQty  float64 //initial order qty
+	OrderFilledQty float64 //event executed qty
+	Status         string  //used in bot.getOrderData
+}
