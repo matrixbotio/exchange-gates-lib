28d3c1cb580a6794c964272069e04cb6e7d90561
Sagleft <sagleft@inbox.ru>
2021-04-16 21:25:50 +0300

    change errors package name

--- interface.go
+++ interface.go
@@ -1,15 +1,17 @@
 package matrixgates
 
-import "github.com/matrixbotio/errors"
+import (
+	sharederrs "github.com/matrixbotio/shared-errors"
+)
 
 //ExchangeInterface - universal exchange adapter interface
 type ExchangeInterface interface {
-	getOrderData() (*TradeEventData, *errors.APIError)
-	placeOrder(order BotOrder) (*struct{}, *errors.APIError)
-	getAccountData() (*struct{}, *errors.APIError)
-	getPairLastPrice() (float64, *errors.APIError)
-	cancelPairOrder() *errors.APIError
-	cancelPairOrders() *errors.APIError
-	getPairOpenOrders() ([]*struct{}, *errors.APIError)
-	verifyAPIKeys() *errors.APIError
+	getOrderData() (*TradeEventData, *sharederrs.APIError)
+	placeOrder(order BotOrder) (*struct{}, *sharederrs.APIError)
+	getAccountData() (*struct{}, *sharederrs.APIError)
+	getPairLastPrice() (float64, *sharederrs.APIError)
+	cancelPairOrder() *sharederrs.APIError
+	cancelPairOrders() *sharederrs.APIError
+	getPairOpenOrders() ([]*struct{}, *sharederrs.APIError)
+	verifyAPIKeys() *sharederrs.APIError
 }
