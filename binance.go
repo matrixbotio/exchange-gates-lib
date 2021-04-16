6ff5853d1165e206fa357d818706789bd3dde08b
Sagleft <sagleft@inbox.ru>
2021-04-16 23:58:40 +0300

    create method placeholders

--- binance.go
+++ binance.go
@@ -12,3 +12,45 @@ func (a *BinanceSpotAdapter) GetOrderData() (*TradeEventData, *sharederrs.APIErr
 	//TODO
 	return nil, nil
 }
+
+//PlaceOrder ..
+func (a *BinanceSpotAdapter) PlaceOrder(order BotOrder) (*struct{}, *sharederrs.APIError) {
+	//TODO
+	return nil, nil
+}
+
+//GetAccountData ..
+func (a *BinanceSpotAdapter) GetAccountData(order BotOrder) (*struct{}, *sharederrs.APIError) {
+	//TODO
+	return nil, nil
+}
+
+//GetPairLastPrice ..
+func (a *BinanceSpotAdapter) GetPairLastPrice() (float64, *sharederrs.APIError) {
+	//TODO
+	return 0, nil
+}
+
+//CancelPairOrder ..
+func (a *BinanceSpotAdapter) CancelPairOrder() *sharederrs.APIError {
+	//TODO
+	return nil
+}
+
+//CancelPairOrders ..
+func (a *BinanceSpotAdapter) CancelPairOrders() *sharederrs.APIError {
+	//TODO
+	return nil
+}
+
+//GetPairOpenOrders ..
+func (a *BinanceSpotAdapter) GetPairOpenOrders() ([]*struct{}, *sharederrs.APIError) {
+	//TODO
+	return nil, nil
+}
+
+//VerifyAPIKeys ..
+func (a *BinanceSpotAdapter) VerifyAPIKeys() *sharederrs.APIError {
+	//TODO
+	return nil
+}
