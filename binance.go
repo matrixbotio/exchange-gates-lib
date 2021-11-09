e435f15b3d98facb3fd54f5332109edf8815b061
Sagleft <sagleft@mfc.click>
2021-11-09 13:23:09 +0200

    update min deposit const

--- binance.go
+++ binance.go
@@ -13,11 +13,12 @@ import (
 )
 
 const (
-	pairDefaultMinQty    = 10
+	pairDefaultMinQty    = 0.001
 	pairDefaultMaxQty    = 99999.99
 	pairDefaultMinPrice  = 0.000001
 	pairDefaultQtyStep   = 0.001
 	pairDefaultPriceStep = 0.000001
+	pairMinDeposit       = 10 // 
 
 	candlesInterval = "1m"
 )
