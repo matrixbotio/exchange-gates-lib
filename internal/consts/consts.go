6e366f2638e551e74a7f42d70c551208ebcf4a31
Sagleft <sagleft@inbox.ru>
2022-11-21 23:20:37 +0200

    update min deposit

--- internal/consts/consts.go
+++ internal/consts/consts.go
@@ -3,18 +3,17 @@ package consts
 import "time"
 
 const (
-	TestExchangeID                = 0
-	PairDefaultExchangeID         = 1
-	PairDefaultMinQty             = 0.001
-	PairDefaultMaxQty             = 99999.99
-	PairDefaultMinPrice           = 0.000001
-	PairDefaultQtyStep            = 0.001
-	PairDefaultPriceStep          = 0.000001
-	PairMinDeposit                = 10
-	PairDefaultBaseAsset          = "BTC"
-	PairDefaultQuoteAsset         = "BUSD"
-	PairDefaultAsset              = PairDefaultBaseAsset + PairDefaultQuoteAsset
-	MinDepositFix         float64 = 10 // percent
+	TestExchangeID        = 0
+	PairDefaultExchangeID = 1
+	PairDefaultMinQty     = 0.001
+	PairDefaultMaxQty     = 99999.99
+	PairDefaultMinPrice   = 0.000001
+	PairDefaultQtyStep    = 0.001
+	PairDefaultPriceStep  = 0.000001
+	PairMinDeposit        = 10
+	PairDefaultBaseAsset  = "BTC"
+	PairDefaultQuoteAsset = "BUSD"
+	PairDefaultAsset      = PairDefaultBaseAsset + PairDefaultQuoteAsset
 
 	CandlesInterval          = "1m"
 	ExchangeSetupConnTimeout = 3500 // ms
