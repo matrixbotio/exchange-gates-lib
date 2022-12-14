103a7f16c702c6c24588c7220dd35e70ccc8d584
Sagleft <sagleft@inbox.ru>
2022-12-14 23:11:29 +0400

    update `RoundFloatFloor`
    fix frac
    
    round with lib

--- go.mod
+++ go.mod
@@ -13,6 +13,7 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/klauspost/compress v1.13.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
 	gopkg.in/yaml.v2 v2.2.8 // indirect
 	nhooyr.io/websocket v1.8.7 // indirect
--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -1,11 +1,13 @@
 package utils
 
 import (
+	"fmt"
 	"strconv"
 	"testing"
 
 	"github.com/matrixbotio/exchange-gates-lib/internal/structs"
 	pkgStructs "github.com/matrixbotio/exchange-gates-lib/pkg/structs"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -43,10 +45,10 @@ func TestOrderResponseToBotOrder(t *testing.T) {
 
 func TestRoundPairOrderValues(t *testing.T) {
 	originalOrder := pkgStructs.BotOrder{
-		Qty:   0.666666666666,
-		Price: 100.66666666666666,
+		Qty:     0.666666666666,
+		Price:   100.66666666666666,
+		Deposit: 67.111111111044,
 	}
-	originalOrder.Deposit = originalOrder.Qty * originalOrder.Price
 
 	pairData := structs.ExchangePairData{
 		BaseAsset:          "ETH",
@@ -69,9 +71,32 @@ func TestRoundPairOrderValues(t *testing.T) {
 	parsedOrder, err := ParseAdjustedOrder(roundedOrder)
 	require.Nil(t, err)
 
-	require.LessOrEqual(t, parsedOrder.Deposit, originalOrder.Deposit)
+	assert.Equal(t, 0.6666, parsedOrder.Qty)
+	assert.Equal(t, 100.66, parsedOrder.Price)
+	assert.LessOrEqual(t, parsedOrder.Deposit, originalOrder.Deposit)
 }
 
 func TestFormatFloatFloor(t *testing.T) {
-	require.Equal(t, "0.11", formatFloatFloor(0.111, 2))
+	require.Equal(t, "0.00056", formatFloatFloor(0.00056, 5))
+}
+
+func TestRoundFloatFloor(t *testing.T) {
+	var val float64 = 0.00053
+	assert.Equal(t, val, RoundFloatFloor(val, 5))
+
+	val = 0.00056
+	assert.Equal(t, val, RoundFloatFloor(val, 5))
+
+	val = 0.666666666666
+	assert.Equal(t, 0.6666, RoundFloatFloor(val, 4))
+}
+
+func TestRoundOrderQty(t *testing.T) {
+	var orderQty float64 = 0.00056
+	var qtyStep float64 = 0.00001
+	var qtyPrecision int = GetFloatPrecision(qtyStep)
+	assert.Equal(t, 5, qtyPrecision)
+
+	roundedQty := formatFloatFloor(orderQty, qtyPrecision)
+	assert.Equal(t, fmt.Sprintf("%v", orderQty), roundedQty)
 }
