ad29de249a4f4d03f7f2bb8e6b068cf89e30ce49
Sagleft <sagleft@inbox.ru>
2023-09-20 10:57:41 +0300

    update cancel bybit order error handling

---
+++ internal/adapters/bybit/helpers/errs/helpers.go
@@ -0,0 +1,30 @@
+package errs
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/matrixbotio/exchange-gates-lib/pkg/errs"
+)
+
+func HandleCancelOrderError(
+	orderIDFormatted string,
+	pairSymbol string,
+	err error,
+) error {
+	if err == nil {
+		return nil
+	}
+
+	if strings.Contains(err.Error(), "Order has been canceled") {
+		return nil
+	}
+	if strings.Contains(err.Error(), errs.ErrOrderFilled.Error()) {
+		return errs.ErrOrderFilled
+	}
+
+	return fmt.Errorf(
+		"cancel order %s in %q: %w",
+		orderIDFormatted, pairSymbol, err,
+	)
+}
