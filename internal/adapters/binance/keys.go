d309145f58d01cba6b521b29eb45e9da7bf9a5b6
Sagleft <sagleft@inbox.ru>
2023-12-09 16:20:55 +0300

    fix: binance API key verfiy

--- internal/adapters/binance/keys.go
+++ internal/adapters/binance/keys.go
@@ -5,9 +5,20 @@ import (
 	"fmt"
 
 	"github.com/matrixbotio/exchange-gates-lib/internal/adapters/binance/helpers/errs"
+	"github.com/matrixbotio/exchange-gates-lib/pkg/structs"
 )
 
 func (a *adapter) VerifyAPIKeys(keyPublic, keySecret string) error {
+	if err := a.Connect(structs.APICredentials{
+		Type: structs.APICredentialsTypeKeypair,
+		Keypair: structs.APIKeypair{
+			Public: keyPublic,
+			Secret: keySecret,
+		},
+	}); err != nil {
+		return fmt.Errorf("binance connect: %w", err)
+	}
+
 	accountData, err := a.binanceAPI.GetAccountData(context.Background())
 	if err != nil {
 		return fmt.Errorf("invalid api key: %w", err)
