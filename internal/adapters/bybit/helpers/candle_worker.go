1881680156854cc12b50b54dfcd89f9e70380f6b
Nikita Zhdanov <wsleepwalker@gmail.com>
2024-02-16 09:50:17 +0100

    Fix bybit unsubscribe goroutine leak Implement bybit candles worker unsubscribe handler Fix bybit private trade events w.WsChannels nil pointer reference

--- internal/adapters/bybit/helpers/candle_worker.go
+++ internal/adapters/bybit/helpers/candle_worker.go
@@ -3,6 +3,7 @@ package helpers
 import (
 	"context"
 	"fmt"
+	pkgStructs "github.com/matrixbotio/exchange-gates-lib/pkg/structs"
 
 	"github.com/hirokisan/bybit/v2"
 	"github.com/matrixbotio/exchange-gates-lib/internal/adapters/bybit/helpers/mappers"
@@ -19,6 +20,10 @@ func (w *CandleEventWorkerBybit) SubscribeToCandle(
 	eventCallback func(event workers.CandleEvent),
 	errorHandler func(err error),
 ) error {
+	w.WsChannels = new(pkgStructs.WorkerChannels)
+	w.WsChannels.WsStop = make(chan struct{}, 1)
+	w.WsChannels.WsDone = make(chan struct{}, 1)
+
 	wsSrv, err := w.WsClient.V5().Public(bybit.CategoryV5Spot)
 	if err != nil {
 		return fmt.Errorf("create trade events subscription service: %w", err)
@@ -34,22 +39,38 @@ func (w *CandleEventWorkerBybit) SubscribeToCandle(
 		return fmt.Errorf("interval %q not available", defaultCandleInterval)
 	}
 
-	if _, err := wsSrv.SubscribeKline(bybit.V5WebsocketPublicKlineParamKey{
+	unsubscribe, err := wsSrv.SubscribeKline(bybit.V5WebsocketPublicKlineParamKey{
 		Interval: bybit.Interval(bybitInterval.Code),
 		Symbol:   bybit.SymbolV5(pairSymbol),
-	}, eventHandler.handle); err != nil {
+	}, eventHandler.handle)
+	if err != nil {
 		return fmt.Errorf("open candle events subscription: %w", err)
 	}
+
+	go func() {
+		select {
+		case <-w.WsChannels.WsStop:
+			if err := unsubscribe(); err != nil {
+				errorHandler(fmt.Errorf("unsubscribe from ticker events: %w", err))
+			}
+		case <-w.WsChannels.WsDone:
+		}
+	}()
 
 	wsErrHandler := func(isWebsocketClosed bool, err error) {
 		if !isWebsocketClosed {
 			_ = wsSrv.Close()
 		}
+
+		w.WsChannels.WsDone <- struct{}{}
+
 		errorHandler(fmt.Errorf("bybit candles subscription: %w", err))
 	}
 
 	go func() {
 		if err := wsSrv.Start(context.Background(), wsErrHandler); err != nil {
+			w.WsChannels.WsDone <- struct{}{}
+
 			errorHandler(fmt.Errorf(
 				"start candle %q events subscription: %w",
 				pairSymbol, err,
