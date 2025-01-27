60cd9778def43a1b8ce738b432c3c11684187231
Sagleft <sagleft@inbox.ru>
2025-01-27 17:46:05 +0300

    export worker mocks (#261)
    * export worker mocks
    
    * export mock constructors

--- pkg/worker/structs.go
+++ pkg/worker/structs.go
@@ -4,4 +4,14 @@ import "github.com/matrixbotio/exchange-gates-lib/internal/workers"
 
 type (
 	CandleEvent = workers.CandleEvent
+
+	MockCandleWorker = workers.MockICandleWorker
+	MockTradeWorker  = workers.MockITradeEventWorker
+	MockPriceWorker  = workers.MockIPriceWorker
+)
+
+var (
+	NewMockCandleWorker = workers.NewMockICandleWorker
+	NewMockTradeWorker  = workers.NewMockITradeEventWorker
+	NewMockPriceWorker  = workers.NewMockIPriceWorker
 )
