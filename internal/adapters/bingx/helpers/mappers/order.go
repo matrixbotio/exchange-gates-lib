f0d1732b2ea7f25049ea3026ac233f60e3264681
Sagleft <sagleft@inbox.ru>
2025-03-05 02:17:15 +0300

    update order converter

--- internal/adapters/bingx/helpers/mappers/order.go
+++ internal/adapters/bingx/helpers/mappers/order.go
@@ -105,6 +105,46 @@ func ConvertBingXOrderData(data *bingxgo.SpotOrder) (structs.OrderData, error) {
 	}, nil
 }
 
+func ConvertBingXHistoryOrder(data bingxgo.HistoryOrder) (structs.OrderData, error) {
+	orderPrice, err := strconv.ParseFloat(data.Price, 64)
+	if err != nil {
+		return structs.OrderData{}, fmt.Errorf("price: %w", err)
+	}
+
+	orderStatus, err := ConvertBingXStatus(data.Status)
+	if err != nil {
+		return structs.OrderData{}, fmt.Errorf("convert status: %w", err)
+	}
+
+	orderQty, err := strconv.ParseFloat(data.OrigQty, 64)
+	if err != nil {
+		return structs.OrderData{}, fmt.Errorf("qty: %w", err)
+	}
+
+	orderFilledQty, err := strconv.ParseFloat(data.ExecutedQty, 64)
+	if err != nil {
+		return structs.OrderData{}, fmt.Errorf("filled qty: %w", err)
+	}
+
+	orderSide, err := ConvertBingXSide(data.Side)
+	if err != nil {
+		return structs.OrderData{}, fmt.Errorf("convert side: %w", err)
+	}
+
+	return structs.OrderData{
+		OrderID:       data.OrderID,
+		ClientOrderID: data.ClientOrderID,
+		Status:        orderStatus,
+		AwaitQty:      orderQty,
+		FilledQty:     orderFilledQty,
+		Price:         orderPrice,
+		Symbol:        data.Symbol,
+		Side:          orderSide,
+		CreatedTime:   data.Time,
+		UpdatedTime:   data.UpdateTime,
+	}, nil
+}
+
 func ConvertOrderResponse(
 	r *bingxgo.SpotOrderResponse,
 ) (structs.CreateOrderResponse, error) {
--- internal/adapters/bingx/order.go
+++ internal/adapters/bingx/order.go
@@ -82,25 +82,31 @@ func (a *adapter) GetOrderExecFee(
 		return structs.OrderFees{}, errors.New("order data not set")
 	}
 
-	return getFeesFromOrderData(orderSide, data.Fee), nil
+	feeValue, err := decimal.NewFromString(data.Fee)
+	if err != nil {
+		return structs.OrderFees{}, fmt.Errorf("parse fee: %w", err)
+	}
+
+	return getFeesFromOrderData(
+		orderSide,
+		feeValue.Abs(),
+	), nil
 }
 
 func getFeesFromOrderData(
 	orderSide consts.OrderSide,
-	fee float64,
+	fee decimal.Decimal,
 ) structs.OrderFees {
 	fees := structs.OrderFees{
 		BaseAsset:  decimal.Zero,
 		QuoteAsset: decimal.Zero,
 	}
 
-	feeVal := decimal.NewFromFloat(fee).Abs()
-
 	if orderSide == consts.OrderSideBuy {
-		fees.BaseAsset = feeVal
+		fees.BaseAsset = fee
 	}
 	if orderSide == consts.OrderSideSell {
-		fees.QuoteAsset = feeVal
+		fees.QuoteAsset = fee
 	}
 	return fees
 }
@@ -130,14 +136,17 @@ func (a *adapter) GetHistoryOrder(
 		return structs.OrderHistory{}, fmt.Errorf("get: %w", err)
 	}
 
-	orderData, err := mappers.ConvertBingXOrderData(&order)
+	orderData, err := mappers.ConvertBingXHistoryOrder(order)
 	if err != nil {
 		return structs.OrderHistory{}, fmt.Errorf("convert: %w", err)
 	}
 
 	return structs.OrderHistory{
 		OrderData: orderData,
-		Fees:      getFeesFromOrderData(orderData.Side, order.Fee),
+		Fees: getFeesFromOrderData(
+			orderData.Side,
+			decimal.NewFromFloat(order.Fee),
+		),
 	}, nil
 }
 
